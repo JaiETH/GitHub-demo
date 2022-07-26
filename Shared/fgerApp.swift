10391fd0f32c58446047ce1b3367839f09a55a17
Bartosz Maludzinski <bartosz.maludzinski@gmail.om>
2022-07-26 13:56:47 +0200

    Initial Commit

---
+++ Shared/fgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  fgerApp.swift
+//  Shared
+//
+//  Created by Bartosz Maludzinski on 26/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct fgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
